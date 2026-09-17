841cfc6e1194204d0483ab21b85798cca70c719b
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-09-17 12:03:00 -0500

    test: RED - prueba A2 lanza excepcion con tiro negativo

---
+++ src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -0,0 +1,21 @@
+﻿package edu.eci.dosw.bowling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
+    }
+
+    @Test
+    @DisplayName("A2: roll(-1) lanza IllegalArgumentException")
+    void testA2_rollNegative() {
+        assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
+    }
+}
