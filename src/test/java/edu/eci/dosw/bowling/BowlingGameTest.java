8f734de2e521b34059f153edb32a021ae3549327
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-09-17 13:39:17 -0500

    feat: TDD - añade suite completa de pruebas e incluye prueba B4 juego perfecto

--- src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -1,4 +1,4 @@
-package edu.eci.dosw.bowling;
+﻿package edu.eci.dosw.bowling;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -42,7 +42,7 @@ class BowlingGameTest {
     }
 
     @Test
-    @DisplayName("B1: Calcular puntaje para una partida abierta sin chuzas ni spares")
+    @DisplayName("B1: Calcular puntaje para una partida abierta")
     void testB1_openGameScore() {
         for (int i = 0; i < 10; i++) {
             game.roll(1);
@@ -55,14 +55,32 @@ class BowlingGameTest {
     @DisplayName("B2: Un spare otorga bono del siguiente lanzamiento")
     void testB2_spareScore() {
         game.roll(5);
-        game.roll(5); // Spare en el frame 1
-        game.roll(3); // Tiro de bono = 3
-        game.roll(0);
-        // Resto de tiros en 0
+        game.roll(5);
+        game.roll(3);
+        for (int i = 0; i < 17; i++) {
+            game.roll(0);
+        }
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    @DisplayName("B3: Un strike otorga bono de los dos siguientes lanzamientos")
+    void testB3_strikeScore() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
         for (int i = 0; i < 16; i++) {
             game.roll(0);
         }
-        // Frame 1: 10 + 3 = 13. Frame 2: 3 + 0 = 3. Total = 16.
-        assertEquals(16, game.score());
+        assertEquals(24, game.score());
+    }
+
+    @Test
+    @DisplayName("B4: Juego perfecto obtiene una puntuacion de 300")
+    void testB4_perfectGame() {
+        for (int i = 0; i < 12; i++) {
+            game.roll(10);
+        }
+        assertEquals(300, game.score());
     }
 }
