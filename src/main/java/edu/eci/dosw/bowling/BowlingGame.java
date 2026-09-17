c46b8a7e76ba1d162af09d98455246e16ec14ff2
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-09-17 12:40:05 -0500

    feat: GREEN - limita el maximo de pinos por tiro a 10

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -2,8 +2,8 @@ package edu.eci.dosw.bowling;
 
 public class BowlingGame {
     public void roll(int pins) {
-        if (pins < 0) {
-            throw new IllegalArgumentException("Los pinos no pueden ser negativos");
+        if (pins < 0 || pins > 10) {
+            throw new IllegalArgumentException("El numero de pinos debe estar entre 0 y 10");
         }
     }
 }
